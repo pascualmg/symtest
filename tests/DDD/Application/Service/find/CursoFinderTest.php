78472ab07a340891e3a83b229f44dbea22f51843
pascualmg <pascual.munoz@pccomponentes.com>
2019-12-13 00:59:50 +0100

    test testing

---
+++ tests/DDD/Application/Service/find/CursoFinderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\DDD\Application\Service\find;
+
+use App\DDD\Application\Service\find\CursoFinder;
+
+class CursoFinderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGivenFinderWhenSearchAndExistsGetACurso(): void
+    {
+
+    }
+
+
+}
